bd1ef85eba4b0988418b21f1442c39a9e4d08ea6
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-01-28 15:54:58 +0100

    Implement player and computer move logic with result display

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
-//komputer
-let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+function getMoveName(argMoveId) {
+    console.log('wywolanie getMoveName z argumentami', argMoveId);
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nieznany ruch';
+    }
+}
+
+
+function displayResult(argComputerMove, argPlayerMove) {
+    console.log('moves', argComputerMove, argPlayerMove);
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+    if( argComputerMove == argPlayerMove){
+      printMessage('Remis!');
+    } else if (
+        (argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+        (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
+        (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')
+    ){
+      printMessage('Ty wygyrwasz!');
+    } else if (argPlayerMove == 'nieznany ruch') {
+        printMessage('Nieznany ruch');
+    } else {
+        printMessage('Przegrales!')
+    }
+  }
+
+clearMessages();
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
+printMessage('ruch komputera to: ' + computerMove);
+
+/*let computerMove = 'nieznany ruch';
 
 if(randomNumber === 1) {
     computerMove = 'kamień';
@@ -13,14 +51,21 @@ if(randomNumber === 1) {
     computerMove = 'nożyce';
 }
 
-printMessage('Ruch komputera to: ' + computerMove);
+printMessage('Ruch komputera to: ' + computerMove);*/
 
 //gracz
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(parseInt(playerInput));
+if (playerMove == 'nieznany ruch') {
+    printMessage('Brawo znasz wiecej cyfr niz 3, ale w grze są 3 możliwości wyboru!');
+} else {
+    printMessage('Twoj ruch to: ' + playerMove);
+
+    displayResult(computerMove, playerMove)
+}
+/*let playerMove = 'nieznany ruch';
 
 if(playerInput === '1') {
   playerMove = 'kamień';
@@ -32,11 +77,11 @@ if(playerInput === '1') {
     printMessage('Brawo umiesz liczyć do 4, ale w grze są 3 możliwości wyboru!');
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove);*/
 
 //wynik
 
-if (computerMove === playerMove) {
+/*if (computerMove === playerMove) {
     printMessage('Remis!');
 } else if (computerMove === 'kamień' && playerMove === 'papier') {
     printMessage('Ty wygrywasz!');
@@ -48,9 +93,9 @@ if (computerMove === playerMove) {
     printMessage('Nieznany ruch, nie mozna okreslic wyniku gry');
 } else {
     printMessage('Przegrales, a komputer wygrywa!');
-}
+} */
 
-if(playerInput == '1') {
+/* if(playerInput == '1') {
   playerMove = 'kamień';
 } else if (playerInput == '2') {
     playerMove = 'papier';
@@ -60,4 +105,4 @@ if(playerInput == '1') {
     printMessage('Brawo umiesz liczyc do 4 ale w grze sa 3 mozliwosci wyboru!')
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove); */
